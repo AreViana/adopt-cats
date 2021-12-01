e0f49c7fa3db2e1cc7f2250cfb1ee1479b4607e0
Arely Viana <arely@fullstacklabs.co>
2021-12-01 05:33:31 -0600

    feat: add global interceptor to map response

---
+++ src/common/interceptors/response-mapper.interceptor.ts
@@ -0,0 +1,20 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable()
+export class ResponseMapperInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      map((data) => {
+        const type = Array.isArray(data) ? 'Collection' : 'Object';
+        return { data, metadata: { type } };
+      }),
+    );
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { ResponseMapperInterceptor } from './common/interceptors/response-mapper.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalInterceptors(new ResponseMapperInterceptor());
   await app.listen(3000);
 }
 bootstrap();
