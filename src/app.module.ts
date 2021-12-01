accf6eecfb4b30ed79136483e1791000f1aeafaf
Arely Viana <arely@fullstacklabs.co>
2021-12-01 01:07:52 -0600

    feat: nest g resource cats

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { typeOrmConfig } from './config/typeorm.config';
+import { CatsModule } from './cats/cats.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot(typeOrmConfig()),
+    CatsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
